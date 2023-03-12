91ea2cdd9d7055ec90768158d5f9f548cd5ee453
cloud-ya <claudia.huegi@gmail.com>
2023-03-12 15:22:17 +0100

    Styled app according to Google template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           Coded with 🤍 by{" "}
           <a href="https://cozy-manatee-c79f41.netlify.app/">Claudia Hügi</a>{" "}
