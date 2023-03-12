89892c4b3263ee28790eaecbd217feafbebe2a90
cloud-ya <claudia.huegi@gmail.com>
2023-03-12 14:36:42 +0100

    Imported Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Coded with 🤍 by{" "}
-        <a href="https://cozy-manatee-c79f41.netlify.app/">Claudia Hügi</a> {""}
-        and open sourced on{" "}
-        <a
-          href="https://github.com/cloud-ya/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Coded with 🤍 by{" "}
+          <a href="https://cozy-manatee-c79f41.netlify.app/">Claudia Hügi</a>{" "}
+          {""}
+          and open sourced on{" "}
+          <a
+            href="https://github.com/cloud-ya/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
