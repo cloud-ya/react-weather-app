4b590652cb36b9ee3ee94fbfb63ae6f90cfc21af
cloud-ya <claudia.huegi@gmail.com>
2023-03-12 14:18:33 +0100

    Added footer with portfolio and GitHub repo

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        Coded with 🤍 by{" "}
+        <a href="https://cozy-manatee-c79f41.netlify.app/">Claudia Hügi</a> {""}
+        and open sourced on{" "}
+        <a
+          href="https://github.com/cloud-ya/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
